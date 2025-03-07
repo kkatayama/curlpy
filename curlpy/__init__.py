1529c55798fcffaab23089442f78dd2cab1620a1
Teddy Katayama <katayama@udel.edu>
2025-03-07 01:22:40 -0500

    updated with 'argparse.REMAINDER' method

--- curlpy/__init__.py
+++ curlpy/__init__.py
@@ -3,7 +3,8 @@ import subprocess
 
 def convertCurl(curl):
     src = '/Users/katayama/Documents/Fun/NodeJS/Convert_Curl_to_Python/curl_to_python.js'
-    cmd = '{} --curl "{}"'.format(src, curl)
+    cmd = src + ' --curl ' + curl
+    # cmd = '{} --curl "{}"'.format(src, curl)
     out = subprocess.check_output(cmd, shell=True).decode('utf-8')
     return out
 
--- curlpy/command_line.py
+++ curlpy/command_line.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 from . import convertCurl
 import argparse
+import shlex
 import os
 
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('curl_cmd', help='the curl command to convert into python syntax')
     ap.add_argument('-o', '--output', default=False, help='save output to file')
+    ap.add_argument('curl_cmd', nargs=argparse.REMAINDER, help='the curl command to convert into python syntax')
     args = ap.parse_args()
 
     out_file = args.output
-    curl_cmd = args.curl_cmd
-    if curl_cmd[0] == "'":
-        curl_cmd = curl_cmd.replace('"', '\\"').replace("'", '"')
+    curl_cmd = ''.join(args.curl_cmd) if len(args.curl_cmd) == 1 else ' '.join(args.curl_cmd)
+    # curl_cmd = args.curl_cmd
+    # if curl_cmd[0] == "'":
+    #     curl_cmd = curl_cmd.replace('"', '\\"').replace("'", '"')
 
     py_src = convertCurl(curl_cmd)
     if not out_file:
