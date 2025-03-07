3442e8e690d4f6043149140b2741cf244eb3d8c7
Teddy Katayama <katayama@udel.edu>
2025-03-07 01:25:56 -0500

    updated with 'argparse.REMAINDER' method

--- curlpy/__init__.py
+++ curlpy/__init__.py
@@ -1,10 +1,16 @@
 import subprocess
 
 
-def convertCurl(curl):
+def convertCurl(curl, verbose):
     src = '/Users/katayama/Documents/Fun/NodeJS/Convert_Curl_to_Python/curl_to_python.js'
     cmd = src + ' --curl ' + curl
     # cmd = '{} --curl "{}"'.format(src, curl)
+
+    if verbose:
+        print(f'src = {src}')
+        print(' --curl ')
+        print(f'cmd = {cmd}')
+
     out = subprocess.check_output(cmd, shell=True).decode('utf-8')
     return out
 
--- curlpy/command_line.py
+++ curlpy/command_line.py
@@ -7,17 +7,19 @@ import os
 
 def main():
     ap = argparse.ArgumentParser()
+    ap.add_argument('-v', '--verbose', default=False, help='verbose mode')
     ap.add_argument('-o', '--output', default=False, help='save output to file')
     ap.add_argument('curl_cmd', nargs=argparse.REMAINDER, help='the curl command to convert into python syntax')
     args = ap.parse_args()
 
+    verbose = args.verbose
     out_file = args.output
     curl_cmd = ''.join(args.curl_cmd) if len(args.curl_cmd) == 1 else ' '.join(args.curl_cmd)
     # curl_cmd = args.curl_cmd
     # if curl_cmd[0] == "'":
     #     curl_cmd = curl_cmd.replace('"', '\\"').replace("'", '"')
 
-    py_src = convertCurl(curl_cmd)
+    py_src = convertCurl(curl_cmd, verbose)
     if not out_file:
         print(py_src)
     else:
